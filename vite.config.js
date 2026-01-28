4ae3ed112ac3f8e56922756c1634e1c95be11447
Viktor Nemov <v.nemov@kokoc.tech>
2026-01-28 22:32:43 +0300

    fix: правит конфиг

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/phaser-test/',
   build: {
     outDir: 'dist',
+  },
+  rollupOptions: {
+    output: {
+      assetFileNames: (assetInfo) => {
+        let extType = assetInfo.name.split('.').at(1);
+        if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+          extType = 'img';
+        }
+        return `assets/[name]-[hash][extname]`;
+      },
+      chunkFileNames: 'assets/[name]-[hash].js',
+      entryFileNames: 'assets/[name]-[hash].js',
+    }
   },
 });
